38bfbcb03dd858c7bfda669c455b8dd37c06041c
Ayaan Kazerouni <ayaan@vt.edu>
2016-12-09 22:41:07 -0500

    Clusters for each year.

--- src/clustering.R
+++ src/clustering.R
@@ -1,19 +1,28 @@
 # import data
 team.attrs = read.csv('team_attrs.csv')
+team.attrs$date = as.Date(team.attrs$date)
+splits = split(team.attrs, team.attrs$date)
 
 # figure out how many clusters by calculating
 # wss for 1:15 clusters, then plotting them to see
 # at what point we lose benefit of more clusters
-wss = (nrow(team.attrs) - 1) * sum(apply(team.attrs, 2, var))
-for (i in 2:15) wss[i] = sum(kmeans(team.attrs, centers = i)$withinss)
+# wss = (nrow(team.attrs) - 1) * sum(apply(team.attrs, 2, var))
+# for (i in 2:15) wss[i] = sum(kmeans(team.attrs, centers = i)$withinss)
+# 
+# # looking at the trend in within group sum of squares, we choose k = 3
+# plot(1:15, wss, type='b', xlab = 'Number of clusters', ylab='Within groups sum of squares', 
+#      main= 'No. of clusters vs. WSS')
 
-# looking at the trend in within group sum of squares, we choose k = 3
-plot(1:15, wss, type='b', xlab = 'Number of clusters', ylab='Withing groups sum of squares')
-
-# perform kmeans clustering
-clusters = kmeans(team.attrs, 3)
-
-# plot PCA-reduced data, coloured for clusters
-pca = prcomp(team.attrs)
-plot(pca$x[, 1], pca$x[, 2], col=clusters$cluster + 1, 
-     pch=clusters$cluster, xlab = 'First Principal Component', ylab = 'Second Principal Component')
+par(mfrow=c(2,3))
+for (i in 1:length(splits)) {
+  # perform kmeans clustering
+  data = splits[[i]]
+  data = data[, !(names(data) == 'date')]
+  clusters = kmeans(data, 3)
+  
+  # plot PCA-reduced data, coloured for clusters
+  pca = prcomp(data)
+  plot(pca$x[, 1], pca$x[, 2], col=clusters$cluster + 1, 
+       pch=clusters$cluster, xlab = 'PC1', ylab = 'PC2',
+       main = paste('201', i - 1, sep = ''))
+}
