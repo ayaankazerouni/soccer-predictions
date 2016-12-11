fb21630c80f8c015c6e1712c372521e30bff2186
Ayaan Kazerouni <ayaan@vt.edu>
2016-12-11 17:44:32 -0500

    Comments.

--- src/rule-based.R
+++ src/rule-based.R
@@ -1,24 +1,33 @@
 rule.predict = function(win, lose) {
-  if (win == lose) {
-    return(0)
+  if (win == 1 & lose == 0) {
+    # win model thinks you won, lose model thinks you didn't lose
+    return(2) # won
   }
   
   if (win == 0 & lose == 1) {
-    return(0)
+    # win model thinks you didn't win, lose model thinks you lost
+    return(0) # lost
   }
   
-  if (win == 1 & lose == 0) {
-    return(1)
+  if (win == 1 & lose == 1) {
+    # win model thinks you won, lose model thinks you lost
+    return(0) # conflict, go with the lose model, since it's more accurate
+  }
+  
+  if (win == 0 & lose == 0) {
+    # win model thinks you didn't win, lose model thinks you didn't lose
+    return(1) # draw
   }
 }
 
 get.prediction = function(x) {
   win = x[['win.predictions']]
   lose = x[['lose.predictions']]
+  multi = x[['multinomial.predictions']]
   final = rule.predict(win, lose)
   return(final)
 }
 
 predictions = apply(data.test, 1, get.prediction)
-error = mean(data.test$win != predictions)
+error = mean(data.test$outcome != predictions)
 accuracy = 1 - error
